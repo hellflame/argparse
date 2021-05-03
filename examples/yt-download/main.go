4d545444b867957c456720ba9931d05127fa9408
hellflame <hellflamedly@gmail.com>
2021-05-03 22:12:35 +0800

    reformat yt download example

--- examples/yt-download/main.go
+++ examples/yt-download/main.go
@@ -7,26 +7,15 @@ import (
 )
 
 func main() {
-	parser := argparse.NewParser("yt-download", "youtube downloader, download anything on youtube",
-		&argparse.ParserConfig{Usage: "yt-download [OPTIONS] URL [URL...]", EpiLog: "bug report to: xxx@xxx.xx"})
-	showVersion := parser.Flag("", "version",
-		&argparse.Option{Help: "Print program version and exit", Group: "GeneralOptions"})
-	doUpdate := parser.Flag("U", "update", &argparse.Option{
-		Help: "Update this program to latest version. Make sure that you have sufficient permissions (run with sudo if needed)",
-		Group: "GeneralOptions"})
-	listExtractors := parser.Flag("", "list-extractors", &argparse.Option{Help: "List all supported extractors",
-		Group: "GeneralOptions"})
-	search := parser.String("", "default-search", &argparse.Option{Help: "Use this prefix for unqualified URLs. " +
-		"For example \"gvsearch2:\" downloads two videos from google videos for youtube-dl \"large apple\". Use the value " +
-		"\"auto\" to let youtube-dl", Meta: "PREFIX"})
+	parser := argparse.NewParser("yt-download", "youtube downloader, download anything on youtube", &argparse.ParserConfig{Usage: "yt-download [OPTIONS] URL [URL...]", EpiLog: "bug report to: xxx@xxx.xx"})
+	showVersion := parser.Flag("", "version", &argparse.Option{Help: "Print program version and exit", Group: "GeneralOptions"})
+	doUpdate := parser.Flag("U", "update", &argparse.Option{Help: "Update this program to latest version. Make sure that you have sufficient permissions (run with sudo if needed)", Group: "GeneralOptions"})
+	listExtractors := parser.Flag("", "list-extractors", &argparse.Option{Help: "List all supported extractors", Group: "GeneralOptions"})
+	search := parser.String("", "default-search", &argparse.Option{Help: "Use this prefix for unqualified URLs. For example \"gvsearch2:\" downloads two videos from google videos for youtube-dl \"large apple\". Use the value \"auto\" to let youtube-dl", Meta: "PREFIX"})
 
-	playListStart := parser.Int("", "playlist-start",
-		&argparse.Option{Help: "Playlist video to start at (default is 1)",
-			Meta: "NUMBER", Group: "play-list options", Default: "1"})
+	playListStart := parser.Int("", "playlist-start", &argparse.Option{Help: "Playlist video to start at (default is 1)", Meta: "NUMBER", Group: "play-list options", Default: "1"})
 
-	urls := parser.Strings("", "url",
-		&argparse.Option{Help: "youtube links, like 'https://www.youtube.com/watch?v=xxxxxxxx'",
-			Positional: true, Required: true, Validate: func(arg string) error {
+	urls := parser.Strings("", "url", &argparse.Option{Help: "youtube links, like 'https://www.youtube.com/watch?v=xxxxxxxx'", Positional: true, Required: true, Validate: func(arg string) error {
 				if !strings.HasPrefix(arg, "https://") {
 					return fmt.Errorf("url should be start with 'https://'")
 				}
