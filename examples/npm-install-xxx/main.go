2473e0d0296e9b37b022b3db5ede3c0f079ec62e
hellflame <hellflamedly@gmail.com>
2021-05-13 00:15:25 +0800

    add examples using sub-command & positional

---
+++ examples/npm-install-xxx/main.go
@@ -0,0 +1,22 @@
+// this is show case like npm install xxx xx
+// with sub-command and its positional argument
+// run the code like go run main.go install express vue
+package main
+
+import (
+	"fmt"
+	"github.com/hellflame/argparse"
+)
+
+func main() {
+	parser := argparse.NewParser("npm", "test npm install xxx", nil)
+	install := parser.AddCommand("install", "install something", nil)
+	pkgs := install.Strings("", "package", &argparse.Option{Positional: true})
+	if e := parser.Parse(nil); e != nil {
+		fmt.Println(e.Error())
+		return
+	}
+	for _, pkg := range *pkgs {
+		fmt.Println(pkg)
+	}
+}
