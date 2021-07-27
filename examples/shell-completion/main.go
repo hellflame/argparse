f40b8b4a1d7f8e36c25e3fe5a532f8500e87062d
hellflame <hellflamedly@gmail.com>
2021-07-27 08:26:49 +0800

    add error handle show case state

--- examples/shell-completion/main.go
+++ examples/shell-completion/main.go
@@ -2,6 +2,10 @@
 //
 // with a String Optional Argument created binding to variable 'name'
 // and default help support
+//
+// this show case shows how to handle after help or shell script showed,
+// but it's ok not to handle them separately, because their error msg is empty,
+// only one more blank line will be added in the tail output
 package main
 
 import (
