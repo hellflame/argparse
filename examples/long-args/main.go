d61d703adc4c6fe86558b7b082a9a71c3ccc9062
hellflame <hellflamedly@gmail.com>
2022-02-02 21:47:26 +0800

    add long args desc

--- examples/long-args/main.go
+++ examples/long-args/main.go
@@ -4,7 +4,7 @@ import "github.com/hellflame/argparse"
 
 func main() {
 	parser := argparse.NewParser("long-args", "", &argparse.ParserConfig{
-		MaxHeaderLength: 20, //
+		MaxHeaderLength: 20, // if argument header length exceeds 20 characters, argument help message will display on new line with 20 space indent
 	})
 	parser.String("s", "short", &argparse.Option{Help: "this is a short args"})
 	parser.String("m", "medium-size", &argparse.Option{Help: "this is a medium size args"})
